f93b282d5bf1260b622a99ee1ccf594ba078950b
Waranicaton <carlumoli_00@hotmail.com>
2022-10-30 21:56:38 +0000

    haciendo parte 1 ej 3 del lab 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :monsters, only: [:index, :show, :destroy]
+  resources :monsters, only: [:index, :show, :destroy, :new, :create, :edit, :update]
   resources :tweets, only: [:index, :show, :new, :create]
 
   root "tweets#index"
