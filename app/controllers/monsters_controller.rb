39ca692082e4dd7a0db437c4d40892cdb5b79088
Waranicaton <carlumoli_00@hotmail.com>
2022-10-30 22:42:41 +0000

    lab 5 parte 1 completa

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -7,6 +7,36 @@ class MonstersController < ApplicationController
     @monster = Monster.find(params[:id])
   end
 
+  def new
+    @monster = Monster.new
+  end
+
+  def create
+    @monster = Monster.new(monster_params)
+    if @monster.save
+        redirect_to edit_monster_path(@monster.id)
+    else
+        render :new, status: :see_other
+    end
+  end
+
+  def edit
+    @monster = Monster.find(params[:id])
+  end
+
+  def update
+    @monster = Monster.find(params[:id])
+    if @monster.update(monster_params)
+        redirect_to root_path
+    else
+        render :new, status: :see_other
+    end
+  end
+
+  def monster_params
+    params.require(:monster).permit(:name, :description, :phone, :birthdate)
+  end
+
   def destroy
     @monster = Monster.find(params[:id])
     @monster.destroy
